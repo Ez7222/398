c734f773e0037cbf6245d05fa155b536a0aacdd7
Jayden-cuu <jayden.cuu@gmail.com>
2025-08-04 12:21:08 +1000

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,15 @@ import sqlite3
 
 app = Flask(__name__)
 
+# display the homepage
 @app.route('/')
 def home():
     return render_template('homepage.html')
 
+# display the event list
+@app.route('/Eventlist.html')
+def Eventlist():
+    return render_template('Eventlist.html')
 
 def get_events():
     conn = sqlite3.connect('rgsq.db')
