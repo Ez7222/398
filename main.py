c12168c44c6089d29fb098f129fb31119d1671bf
Jayden-cuu <jayden.cuu@gmail.com>
2025-08-05 11:35:09 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
+from flask_sqlalchemy import SQLAlchemy
 import sqlite3
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///events.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date = db.Column(db.String(50), nullable=False)
+    location = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.String(20), nullable=False)
+    image = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+
+with app.app_context():
+    db.create_all()
 
 # display the homepage
 @app.route('/')
@@ -11,7 +27,12 @@ def home():
 # display the event list
 @app.route('/Eventlist.html')
 def Eventlist():
-    return render_template('Eventlist.html')
+    per_page = 6
+    page = request.args.get('page',1,type=int)
+    event_list = [{"title": f"Event {i}"} for i in range(1, 21)]
+    events_paginated = event_list[(page-1)*per_page: page*per_page]
+    total_pages = (len(event_list) + per_page - 1) // per_page
+    return render_template('Eventlist.html',events = events_paginated, page=page, total_pages = total_pages)
 
 def get_events():
     conn = sqlite3.connect('rgsq.db')
@@ -20,3 +41,5 @@ def get_events():
     events = cursor.fetchall()
     conn.close()
     return events
+
+
