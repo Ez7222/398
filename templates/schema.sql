09caaa830934075458a319bf59fb73a86672eb9e
Ez7222 <guanyuwu87@gmail.com>
2025-09-08 15:57:50 +1000

    Delete schema.sql

--- templates/schema.sql
+++
@@ -1,9 +0,0 @@
-create Table events (
-    id Integer primary key generated autoincrement,
-    title text not null,
-    description text,
-    location text,
-    date text not null,
-    price real not null
-    image text,
-);
