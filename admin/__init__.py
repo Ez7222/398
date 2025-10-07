821f64ef1c0e0e262ce1f83d8e85a6ba4ad1d63d
GavinYuu <n11784709@qut.edu.au>
2025-10-07 17:00:42 +1000

    update for admin and staff page

--- admin/__init__.py
+++ admin/__init__.py
@@ -1,6 +1,5 @@
 from flask import Blueprint
 
-# Minimal admin blueprint. Templates 可放在 templates/admin 下（可选）
 admin_bp = Blueprint(
     "admin",
     __name__,
@@ -8,5 +7,4 @@ admin_bp = Blueprint(
     template_folder="../templates"
 )
 
-# route
-from . import views  # noqa: E402,F401
+from . import views  # noqa
